e66b04e4bc0aecf7dfeb4c9f6e5f1cedbd3e90bf
tatertatitotz <chock.tatiana@outlook.com>
2025-10-13 13:30:06 -0600

    adding variables and print

---
+++ Example.py
@@ -0,0 +1,7 @@
+#variables declared
+name = input("Enter Name: ")
+hobby = input("Enter hobby: ")
+yearOfBirth = int(input("Enter year of birth: "))
+
+#Comma between each string and variable
+print("Hello", name ,". I know you like", hobby , "")
