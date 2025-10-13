e41c059b4e1dcd005d07138e98be8bb12f11a4fc
tatertatitotz <chock.tatiana@outlook.com>
2025-10-13 13:33:59 -0600

    Adding all variables to print

--- Example.py
+++ Example.py
@@ -4,4 +4,4 @@ hobby = input("Enter hobby: ")
 yearOfBirth = int(input("Enter year of birth: "))
 
 #Comma between each string and variable
-print("Hello", name ,". I know you like", hobby , "")
+print("Hello", name ,". I know you like", hobby , "and are born in the year", yearOfBirth)
