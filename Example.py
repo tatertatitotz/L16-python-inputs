f4b00777b9525595af1ed29e90767d449d24b0f7
tatertatitotz <chock.tatiana@outlook.com>
2025-10-13 13:44:54 -0600

    implementing f-strings

--- Example.py
+++ Example.py
@@ -3,9 +3,11 @@ name = input("Enter Name: ")
 hobby = input("Enter hobby: ")
 yearOfBirth = int(input("Enter year of birth: "))
 
-#Comma between each string and variable
-print("Hello", name ,". I know you like", hobby , "and are born in the year", yearOfBirth)
-
+#Comma between each string and variable when not writing in f-string.
+print(f"Hello {name}. I know you like {hobby} and are born in the year {yearOfBirth}.")
+ 
 currentYear = int(input("Enter the current year: "))
+
+#Subtracting the variables in order to get the age of the person.
 age = (currentYear - yearOfBirth)
-print("You are", age, "years old.")
+print(f"{name}, you are {age} years old.")
