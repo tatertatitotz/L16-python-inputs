ded4c5f819a6bc752d2159786678524ad814a3de
tatertatitotz <chock.tatiana@outlook.com>
2025-10-13 15:37:48 -0400

    Age version1

--- Example.py
+++ Example.py
@@ -5,3 +5,7 @@ yearOfBirth = int(input("Enter year of birth: "))
 
 #Comma between each string and variable
 print("Hello", name ,". I know you like", hobby , "and are born in the year", yearOfBirth)
+
+currentYear = int(input("Enter the current year: "))
+age = (currentYear - yearOfBirth)
+print("You are", age, "years old.")
