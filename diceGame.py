a44cffbeaa34d8a6ed9f7cd7d699c2e0eb0e12e4
tatertatitotz <chock.tatiana@outlook.com>
2025-10-20 13:21:15 -0600

    added comments

--- diceGame.py
+++ diceGame.py
@@ -1,12 +1,15 @@
 #import random last
 #define die values
-die = 2
-die2 = 3
+die = 6
+die2 = 5
 #define total
 total = die + die2
 
+#If the dice total is 7 OR 11 then you win.
 if total == 7 or total == 11:
     print("You win!")
+#elif
+#Indicates loss! Important, nothing shows up if you lose otherwise!
 else:
     print("You lose")
 
