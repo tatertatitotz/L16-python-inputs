be077d79f95cc9544ee3356711f1388b32bc976b
tatertatitotz <chock.tatiana@outlook.com>
2025-10-20 13:22:39 -0600

    same number test

--- diceGame.py
+++ diceGame.py
@@ -1,14 +1,16 @@
 #import random last
 #define die values
 die = 6
-die2 = 5
+die2 = 6
 #define total
 total = die + die2
 
 #If the dice total is 7 OR 11 then you win.
 if total == 7 or total == 11:
     print("You win!")
-#elif
+#dice have the same number
+elif die == die2:
+    print("Doubles! You win!")
 #Indicates loss! Important, nothing shows up if you lose otherwise!
 else:
     print("You lose")
