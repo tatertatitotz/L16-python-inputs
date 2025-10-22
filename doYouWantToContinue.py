6dfcb386778e04e8ced736b16dc14423f1680aad
tatertatitotz <chock.tatiana@outlook.com>
2025-10-22 12:58:46 -0600

    renamed input to avoid confusion

--- doYouWantToContinue.py
+++ doYouWantToContinue.py
@@ -1,4 +1,5 @@
-input = input("Do you want to continue? Enter y or n: ")
-while input == "y" or "Y":
+enter = input("Do you want to continue? Enter y or n: ")
+while enter == "y" or "Y":
+    enter = input("Do you want to continue? Enter y or n: ")
     print(input)
     
