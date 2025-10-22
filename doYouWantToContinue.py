0160e9b0b531775c8ab9c9d313318c86d0e19cf1
tatertatitotz <chock.tatiana@outlook.com>
2025-10-22 13:37:06 -0600

    finished

--- doYouWantToContinue.py
+++ doYouWantToContinue.py
@@ -1,8 +1,13 @@
 #assigning enter
 enter = input("Do you want to continue? Enter y or n: ")
+#assigning loopCount
+loopCount = 0
 #when input response is y, it asks again.
-while enter == "y" or "Y":
+#ALWAYS DEFINE enter == "y" and enter == "Y" SEPERATELY
+while enter == "y" or enter == "Y":
+    #adding one to loop count every time y is entered
+    loopCount += 1
+    #asks again for the input
     enter = input("Do you want to continue? Enter y or n: ")
-    #
-    if enter == "n" or "N":
-        print("hellooooooooe")
+#gives the total number of times the loop has occured
+print(f"The loop ran {loopCount} times")
