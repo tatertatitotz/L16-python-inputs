017e38d066f0c4e770be9b208c7d47af35a0849d
tatertatitotz <chock.tatiana@outlook.com>
2025-10-23 15:50:48 -0600

    added variables r and p to the win pool

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,13 +1,26 @@
 #must always import random
 import random
+#where randit will go
+computer = 3
 
-computer = 1
 if computer == 1:
     computer = "r"
-    
+if computer == 2:
+    computer = "p"
+if computer == 3:
+    computer = "s"
+
 player = input("rock (r), paper (p) or scissors (s)? Enter: ")
+
+print(f"{player} vs. {computer}")
 
 if player == computer:
     print("Tie!")
 if player == "r" and computer == "p":
+    print("Computer Wins!")
+if player == "r" and computer == "s":
+    print("You Win!")
+if player == "p" and computer == "r":
+    print("You Win!")
+if player == "p" and computer == "s":
     print("Computer Wins!")
