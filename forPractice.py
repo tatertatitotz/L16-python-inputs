c5f9e530157af9eca2fc141b5c1b781f0d6c271e
tatertatitotz <chock.tatiana@outlook.com>
2025-10-27 13:22:14 -0600

    fix range notation so it outputs correct ints.

--- forPractice.py
+++ forPractice.py
@@ -3,7 +3,7 @@ number = int(input("Enter number you want multiplied: "))
 #mult will have a value within the range of (1, 11)
 #mult will be assigned a new value each loop, which is its previous +1
 #mult will end when 11 values have been presented.
-for mult in range(1, 10):
+for mult in range(1, 11):
     #calculating the total based on the number given, and the set multiplied values
     total = number * mult
     #displays the multiplication and the answer
