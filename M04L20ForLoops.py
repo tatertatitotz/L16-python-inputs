d6b6a0e146186592ac30c9da351e10d9cc3118fb
tatertatitotz <chock.tatiana@outlook.com>
2025-10-27 12:50:38 -0600

    Update M04L20ForLoops.py

---
+++ M04L20ForLoops.py
@@ -0,0 +1,10 @@
+#the starting number for range will always be 0. 
+#range(5) means 5 numbers total will be printed, when the variable is printed.
+for count in range(5.3):
+    #prints out 5 numbers total including count's value
+    print(count)
+    #count starts at 0
+    #count variable increments of 1
+    #count stops at 4
+#nothing happens if n is value -5
+#count in range(n) n equals the number of values shown when adding 1 to count
