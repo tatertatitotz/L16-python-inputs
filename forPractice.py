991fc0cbd07b8fddbc4d0f8240f44e58677314a5
tatertatitotz <chock.tatiana@outlook.com>
2025-10-27 13:21:18 -0600

    comments and range fix

--- forPractice.py
+++ forPractice.py
@@ -1,5 +1,10 @@
-number=int(input("Enter number you want multiplied: "))
-for mult in (0, 11):
-    #this is going to break because you're multiplying an int by a set of numbers
+#the user inputs only whole numbers
+number = int(input("Enter number you want multiplied: "))
+#mult will have a value within the range of (1, 11)
+#mult will be assigned a new value each loop, which is its previous +1
+#mult will end when 11 values have been presented.
+for mult in range(1, 10):
+    #calculating the total based on the number given, and the set multiplied values
     total = number * mult
+    #displays the multiplication and the answer
     print(f"{number} x {mult} = {total}")
