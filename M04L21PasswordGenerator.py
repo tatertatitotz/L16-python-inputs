d0b3464e1e5f732120c92de665a6156235ebeca3
tatertatitotz <chock.tatiana@outlook.com>
2025-11-03 13:36:52 -0700

    added comments

--- M04L21PasswordGenerator.py
+++ M04L21PasswordGenerator.py
@@ -6,11 +6,13 @@ currentLength = 0
 length = 4
 
 password = ""
-
+#if the currentLength is shorter than the given length, it repeasts
 while length > currentLength:
-    currentLength += 1
     character = random.choice(char)
+    #adds a character to the password
     password += character
+    #adds one to the current length with each character added
+    currentLength += 1
 print (currentLength)
 
 print(password)
