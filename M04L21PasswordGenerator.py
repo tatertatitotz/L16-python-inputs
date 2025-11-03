be2a94b4363a35ea6773c9224bf6575e3b289da0
tatertatitotz <chock.tatiana@outlook.com>
2025-11-03 13:40:55 -0700

    added count in range

--- M04L21PasswordGenerator.py
+++ M04L21PasswordGenerator.py
@@ -1,10 +1,12 @@
 import random
 
+
 char = 'abcdefghijklmnopqrstuvwxyz'
 currentLength = 0
 
 length = 4
 
+#password must be defined as a string before adding characters
 password = ""
 #if the currentLength is shorter than the given length, it repeasts
 while length > currentLength:
@@ -15,4 +17,6 @@ while length > currentLength:
     currentLength += 1
 print (currentLength)
 
-print(password)
+for count in range(4):
+    print(count)
+
