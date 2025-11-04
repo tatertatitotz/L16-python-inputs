f6d2825fd6440645d327a1b38e116560107c4f9b
tatertatitotz <chock.tatiana@outlook.com>
2025-11-04 12:18:36 -0700

    small version

--- M04L21PasswordGenerator.py
+++ M04L21PasswordGenerator.py
@@ -1,7 +1,7 @@
 import random
 
-
-char = 'abcdefghijklmnopqrstuvwxyz'
+number = 4
+char = "abcdefghijklmnopqrstuvwxyz"
 currentLength = 0
 
 length = 4
@@ -9,14 +9,13 @@ length = 4
 #password must be defined as a string before adding characters
 password = ""
 #if the currentLength is shorter than the given length, it repeasts
-while length > currentLength:
-    character = random.choice(char)
-    #adds a character to the password
-    password += character
-    #adds one to the current length with each character added
-    currentLength += 1
-print (currentLength)
 
-for count in range(4):
-    print(count)
+for password in range(number):
+    password = ""
+    for count in range(length):
+        password += random.choice(char)
+    print(password)
+
+
+
 
