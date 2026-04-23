d8a85718d1f8247a28de91b01b6e9bc4595f07b3
tatertatitotz <chock.tatiana@outlook.com>
2026-04-23 14:28:41 -0600

    Bubble sort begin

---
+++ OwnWork/bubbleSort.py
@@ -0,0 +1,10 @@
+numbers = [8, 2, 5, 3, 1, 7, 4, 6]
+highest = 0
+
+while range[]
+    for count in range(numbers.len()):
+    
+        if numbers[count] > numbers[count+1]:
+            numbers[count+1] = temp
+            numbers[count + 1] = numbers[count]
+            numbers[count]
