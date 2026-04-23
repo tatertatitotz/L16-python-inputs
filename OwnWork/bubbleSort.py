c6092eaa31cbb3931c29d3b8f79cfa6f22989507
tatertatitotz <chock.tatiana@outlook.com>
2026-04-23 14:43:00 -0600

    working bubble sort
    (REFACTOR LATER)

--- OwnWork/bubbleSort.py
+++ OwnWork/bubbleSort.py
@@ -1,10 +1,13 @@
 numbers = [8, 2, 5, 3, 1, 7, 4, 6]
-highest = 0
+sorted = [1, 2, 3, 4, 5, 6, 7, 8]
+temp = 0
 
-while range[]
-    for count in range(numbers.len()):
-    
+while numbers != sorted:
+    for count in range(0, len(numbers)-1):
+        
         if numbers[count] > numbers[count+1]:
-            numbers[count+1] = temp
+            temp = numbers[count+1]
             numbers[count + 1] = numbers[count]
-            numbers[count]
+            numbers[count] = temp
+
+print(numbers)
